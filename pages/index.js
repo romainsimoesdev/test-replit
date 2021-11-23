6762a7f1b24374a29e006e49b9b891ba760d32ab
Romain Simoes <94848512+romainsimoesdev@users.noreply.github.com>
2021-11-23 06:38:01 +0000

    hj

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome toahggha <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
